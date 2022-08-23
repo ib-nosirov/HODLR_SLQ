914d1a1a9460d06760f8dd036ba4547ce0c7b92b
inosirov <ibrohim@nosirov.com>
2022-08-22 23:36:46 -0600

    prep for migration to Exupery.

--- Lanczos.py
+++ Lanczos.py
@@ -4,76 +4,85 @@ import scipy.io
 import HODLR_FMM
 import seaborn as sns; sns.set(context="talk",style="white",palette="pastel")
 import matplotlib.pyplot as plt
-# from here https://scicomp.stackexchange.com/questions/23536/quality-of-eigenvalue-approximation-in-lanczos-method
-def HODLRLanczos(A,v,m=100):
-	n = len(v)
-	if m>n:
-		m = n
-	# from here https://en.wikipedia.org/wiki/Lanczos_algorithm
-	V = np.zeros((m,n))
-	T = np.zeros((m,m))
-	v0 = np.zeros(n)
-	beta = 0
-	for j in range(m-1):
-		w = HODLR_FMM.HODLR_matvec(A,v)
-		alpha = np.dot(w,v)
-		print(alpha)
-		w = w - alpha*v - beta*v0
-		beta = np.sqrt(np.dot(w,w)) 
-		v0 = v
-		v = w/beta 
-		T[j,j] = alpha 
-		T[j,j+1] = beta
-		T[j+1,j] = beta
-		V[j,:] = v
-	w = HODLR_FMM.HODLR_matvec(A,v)
-	alpha = np.dot(w,v)
-	w = w - alpha*v - beta*v0
-	T[m-1,m-1] = np.dot(w,v)
-	V[m-1] = w / np.sqrt(np.dot(w,w)) 
-	return T,V
 
-def Lanczos(A,q,m=100):
-# pass-complexity: w = A@q and is sequential
-# with HODLR, 
-	n = len(q)
-	if m>n:
-		m = n
-	# from here https://en.wikipedia.org/wiki/Lanczos_algorithm
-	V = np.zeros((n,m))
-	T = np.zeros((m,m))
-	q0 = np.zeros(n)
-	beta = 0
-	for j in range(m-1):
-		w = A@q
-		alpha = w@q 
-# w -= alpha*q + beta*q0 may have different effect because += and -= overwrites
-# memory. 
-		w = w - alpha*q - beta*q0
-		beta = np.linalg.norm(w)
-		q0 = q
-		q = w/beta 
-		T[j,j] = alpha 
-		T[j+1,j] = beta
-		T[j,j+1] = beta
-		V[:,j] = q
-#		print(V.T@A@V - T)
-	w = A@q
-	alpha = w@q
-	w = w - alpha*q - beta*q0
-	T[m-1,m-1] = w@q
-	V[:,m-1] = w / np.linalg.norm(w)
-#	print(V)
-#	print(V.T@A@V - T)
-	return T,V
-# 2d array is different than (10,) different than (10,1)
+def HODLRLanczos(A,b,N):
+    c = b / np.linalg.norm(b)
+    Q = np.zeros((N+1,len(b)))
+    Q[0,:] = c
+    alpha = np.zeros([N,])
+    beta = np.zeros([N,])
+    for j in range(N):
+        w = HODLR_FMM.HODLR_matvec(A,Q[j,:])
+        # orthogonalize
+        alpha[j] = np.dot(Q[j,:].T,w)
+        if j==0:
+            qtilde = w - alpha[j]*Q[j,:]
+        else:
+            qtilde = w - alpha[j]*Q[j,:] - beta[j-1]*Q[j-1,:]
+        # normalize
+        beta[j]=np.linalg.norm(qtilde)
+        q = qtilde / beta[j]
+        Q[j+1,:] = q
+    return alpha,beta
 
+def Lanczos(A,b,N):
+    c = b/np.linalg.norm(b)
+    Q = np.zeros((N+1,len(b)))
+    Q[0,:] = c
+    alpha=np.zeros([N,])
+    beta=np.zeros([N,])
+    for j in range(N):
+        w = A@Q[j,:]
+        # orthogonalize
+        alpha[j] = np.dot(Q[j,:].T,w)
+        if j==0:
+            qtilde = w - alpha[j]*Q[j,:]
+        else:
+            qtilde = w - alpha[j]*Q[j,:] - beta[j-1]*Q[j-1,:]
+        # normalize
+        beta[j] = np.linalg.norm(qtilde)
+        q = qtilde / beta[j]
+        Q[j+1,:] = q
+    return alpha,beta
+	
+n = 50
 rng = np.random.default_rng(0)
-A = rng.standard_normal((10,10))
+A = rng.standard_normal((n,n))
 A = A + A.T
-q = rng.standard_normal(10,)
-T,V = Lanczos(A,q,m=10)
-approx_eigs = LA.eig(T)[0]
-print('approx_eigs',approx_eigs)
-eigs = LA.eig(A)[0]
+q = rng.standard_normal(n,)
+alpha,beta = Lanczos(A,q,n)
+T=np.diag(alpha)+np.diag(beta[:-1],-1)+np.diag(beta[:-1],1)
+#approx_eigs = np.linalg.eig(T)[0]
+#print('approx_eigs',np.sort(approx_eigs))
+#eigs = np.real_if_close(np.linalg.eig(A)[0])
+#print('eigs',np.sort(eigs))
+#print(np.sort(approx_eigs)-np.sort(eigs))
+
+def makeA():
+	N = 1000
+	A = np.zeros((N,N))
+	sigma = N/10
+	for ii in range(N):
+		for jj in range(N):
+			A[ii,jj] = np.exp(-(ii-jj)**2/sigma**2)
+	return A
+
+HODLR_mtrx = scipy.io.loadmat('HODLR_mtrx.mat')
+u_tree = HODLR_mtrx['u_tree']
+z_tree = HODLR_mtrx['z_tree']
+leaves_cell = HODLR_mtrx['leaves_cell']
+idx_tree = HODLR_mtrx['idx_tree']
+b = np.ones(2*u_tree[0,1].shape[0])
+A = [u_tree,z_tree,leaves_cell,idx_tree]
+HODLR_eye = HODLR_FMM.HODLR_matvec(A,np.eye(1000)[0,:])
+alpha,beta = HODLRLanczos(A,b,10) 
+T=np.diag(alpha)+np.diag(beta[:-1],-1)+np.diag(beta[:-1],1)
+approx_eigs = np.linalg.eig(T)[0]
+A = makeA()
+eigs = np.real_if_close(scipy.sparse.linalg.eigs(A)[0])
+#print('eigs',eigs)
+approx_eigs = np.sort(approx_eigs)
+eigs = np.sort(eigs)
+print('approx_eigs',approx_eigs[:10])
 print('eigs',eigs)
+#print(approx_eigs[:10]-eigs[:10])
